6b46ff7a7bd6b38953cd96c05af8af9c327a9795
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2017-11-17 18:50:09 +0000

    Create fastq_info.sh

---
+++ fastq_info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+fastq=$1
+
+
+echo -n -e "\taverage_read_length\t"
+echo -e "reads"
+echo -e -n "$fastq\t"
+averagecount=$(awk 'NR % 4 == 2 { s += length($1); t++} END {print s/t}' $fastq)
+echo -e -n "$averagecount\t"
+reads=$(cat $fastq| echo $((`wc -l`/4)))
+echo "$reads"
+
+
+exit 0;
